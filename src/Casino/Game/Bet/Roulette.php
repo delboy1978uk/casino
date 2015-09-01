bb66d4d3c0c3eae3c69eb039743d2c4a50cb382b
delboy1978uk <delboy1978uk@gmail.com>
2015-09-01 13:17:42 +0100

    tidy Bet class

--- src/Casino/Game/Bet/Roulette.php
+++ src/Casino/Game/Bet/Roulette.php
@@ -12,31 +12,22 @@ use Del\Casino\Game\Bet\Type\TypeAbstract as BetType;
 
 class Roulette extends BetAbstract
 {
+    /**
+     * odds:
+     * 'street':12
+     * 'corner':9
+     * 'threeline':12
+     * 'doublestreet':6
+     * 'dozen','column':3
+     * 'half':2
+     *
+     * @param Player $player
+     * @param BetType $type
+     * @param $amount
+     */
     public function __construct(Player &$player, BetType $type, $amount)
     {
         parent::__construct($player, $type, $amount);
         $this->setOdds($type->getOdds());
     }
-
-
-//            case 'street':
-//                $odds = 12;
-//                break;
-//            case 'corner':
-//                $odds = 9;
-//                break;
-//            case 'threeline':
-//                $odds = 12;
-//                break;
-//            case 'doublestreet':
-//                $odds = 6;
-//                break;
-//            case 'dozen':
-//            case 'column':
-//                $odds = 3;
-//                break;
-//            case 'half':
-//                $odds = 2;
-//                break;
-
 }
